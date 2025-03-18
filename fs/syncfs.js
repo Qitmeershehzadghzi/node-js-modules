4d1583051f11dd34cf920166f2f9be29e5f803da
Qetmeer <qitmeershehzad@gmail.com>
2025-03-18 20:45:42 +0500

    additon the sync fs code

---
+++ fs/syncfs.js
@@ -0,0 +1,33 @@
+// write file sync
+const fs =require('fs')
+const path =require('path')
+const filename ='test.txt'
+const filepath = path.join(__dirname, filename);
+// const writefile =fs.writeFileSync(filepath,'hello world updated','utf-8')	
+// console.log(
+//     writefile
+// );
+// read file sync
+
+
+// const readfile = fs.readFileSync(filepath,'utf-8')
+// console.log(readfile);
+
+// updated data
+// const updateddata =fs.appendFileSync(filepath,'\nupdated data','utf-8')
+// console.log(  updateddata);
+
+// delete data
+// const deletedata =fs.unlinkSync(filepath)
+// console.log(deletedata);
+// rename filename
+
+
+
+const newupdatedfilename = 'updatedtest.txt';
+const newfilepath = path.join(__dirname, newupdatedfilename);
+
+// Rename ka kaam
+const renamefile =fs.renameSync(filepath, newfilepath);
+
+console.log('File renamed successfully',renamefile);
